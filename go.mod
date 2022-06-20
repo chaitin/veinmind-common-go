f08502ed58085a830d880c271e58092dfd2d9dd9
d_infinite <463394797@qq.com>
2022-06-20 18:31:37 +0800

    feat: add passwd parser for image

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/chaitin/libveinmind v1.1.1
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
---
+++ passwd/passwd.go
@@ -0,0 +1,18 @@
+package passwd
+
+import (
+	api "github.com/chaitin/libveinmind/go"
+)
+
+const (
+	passwdPath = "/etc/passwd"
+)
+
+func ParseImagePasswd(image api.Image) ([]Entry, error) {
+	f, err := image.Open(passwdPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return ParseReader(f)
+}
