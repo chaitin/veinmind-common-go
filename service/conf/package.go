49eef4af76cf9e2bd2ee9c2713a926a5d3d2a39f
Tsuki124 <876121239@qq.com>
2022-07-21 14:48:55 +0800

    feat(service): add conf and service for weakpass and file-filter plugin

--- service/conf/package.go
+++ service/conf/package.go
@@ -4,5 +4,7 @@ package conf
 type PluginConfNS string
 
 const (
-	Sensitive PluginConfNS = "veinmind-sensitive"
+	Sensitive  PluginConfNS = "veinmind-sensitive"
+	WeakPass   PluginConfNS = "veinmind-weakpass"
+	FileFilter PluginConfNS = "veinmind-file-filter"
 )
