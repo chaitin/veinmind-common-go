11f88c4df92105f5c9a2a2bf91f2a8071780b2b6
ek1ng <761042182@qq.com>
2023-06-21 16:43:53 +0800

    feat: add some common report func of backdoor plugin (#40)

--- service/report/event/backdoor.go
+++ service/report/event/backdoor.go
@@ -10,17 +10,18 @@ func init() {
 
 type BackdoorDetail struct {
 	FileDetail
+	Content     string `json:"content"`
 	Description string `json:"description"`
 }
 
 func (b *BackdoorDetail) RenderTable(id string, level string) []table.Row {
 	data := make([]table.Row, 0)
-	data = append(data, table.Row{simply(id), level, b.CalcSize(), b.Path, b.Description})
+	data = append(data, table.Row{simply(id), level, b.CalcSize(), b.Path, b.Content, b.Description})
 	return data
 }
 
 func (b *BackdoorDetail) RenderTableHeader() table.Row {
-	return table.Row{"FROM", "LEVEL", "Size", "Path", "Description"}
+	return table.Row{"FROM", "LEVEL", "Mod", "FilePath", "Content", "Description"}
 }
 
 func (b *BackdoorDetail) RenderTableTitle() string {
@@ -37,7 +38,7 @@ func (b *BackdoorDetail) RenderColumnConfig() []table.ColumnConfig {
 		{Number: 2, WidthMax: 12},
 		{Number: 3, WidthMax: 12},
 		{Number: 4, WidthMax: 32},
-		{Number: 5, WidthMax: 12},
+		{Number: 5, WidthMax: 32},
 		{Number: 6, WidthMax: 32},
 	}
 }
